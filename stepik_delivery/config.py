57255297e83c06932ad84666a5d851a751a216f4
Ivan Rychkov <rychkov.ivan@gmail.com>
2021-08-17 03:43:12 +0300

    fix for heroku postgres

--- stepik_delivery/config.py
+++ stepik_delivery/config.py
@@ -3,6 +3,6 @@ import os
 
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
